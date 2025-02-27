ed5866c1a70bea9aae5373b205c08febd8f3409d
mlnwns <129190157+mlnwns@users.noreply.github.com>
2025-02-27 14:15:40 +0900

    feat: 폴더 기능 구현 (#162)
    * refactor: 폴더 페이지 추가 및 네비게이션 구조 개선
    
    * refactor: 폴더 타입 헤더 추가 및 타이머 생성 네비게이션 개선
    
    * feat: 폴더 생성 페이지에 폴더 데이터 저장 기능 추가 및 폴더 페이지 생성
    
    * refactor: 폴더 생성 모달에서 알림 제거 및 네비게이션 로직 개선
    
    * feat: 타이머 폴더 클릭 시 네비게이션 추가 및 폴더 ID와 생성 시간 저장 기능 개선
    
    * feat: 폴더 데이터 로드 기능 추가 및 CountdownFolder 컴포넌트에 폴더 데이터 전달
    
    * feat: 폴더에서 제작한 타이머는 폴더에서만 확인 할 수 있도록 구현
    
    * feat: 폴더 클릭 시 최신 항목으로 표시하는 기능 추가
    
    * style: CountdownFolder 컴포넌트의 배경색 투명도 수정

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import TimerUpdatePage from './pages/TimerUpdatePage';
 import Refresh from './pages/Refresh';
 import Login from './pages/Login';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import FolderPage from './pages/FolderPage';
 import {useAppStateMonitor} from './hooks/useAppStateMonitor';
 import {useBackgroundTask} from './hooks/useBackgroundTask';
 
@@ -61,6 +62,11 @@ function App(): React.JSX.Element {
                 options={{title: 'Create Folder Page'}}
               />
               <Stack.Screen
+                name="FolderPage"
+                component={FolderPageWithLayout}
+                options={{title: 'Folder Page'}}
+              />
+              <Stack.Screen
                 name="Timer Update"
                 component={TimerUpdateWithLayout}
                 options={{title: 'Timer Update Page'}}
@@ -95,6 +101,12 @@ function App(): React.JSX.Element {
   );
 }
 
+const FolderPageWithLayout = () => (
+  <BaseLayout>
+    <FolderPage />
+  </BaseLayout>
+);
+
 const MainWithLayout = () => (
   <BaseLayout>
     <MainPage />
@@ -111,11 +123,7 @@ const TimerCreateWithLayout = () => <TimerCreatePage />;
 
 const TimerUpdateWithLayout = () => <TimerUpdatePage />;
 
-const FolderCreateWithLayout = () => (
-  <BaseLayout>
-    <FolderCreatePage />
-  </BaseLayout>
-);
+const FolderCreateWithLayout = () => <FolderCreatePage />;
 
 export default App;
 
