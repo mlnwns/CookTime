4454b1c10e3a4bd1270348da1913ecd42d1f4ce5
mlnwns <mlnwns@naver.com>
2025-04-13 23:49:24 +0900

    refactor: 아이콘 선택 모달 모든 아이콘이 보이도록 개선

--- src/components/modal/iconPickerModal/IconPickerModal.jsx
+++ src/components/modal/iconPickerModal/IconPickerModal.jsx
@@ -17,7 +17,7 @@ const IconPickerModal = ({bottomSheetRef, onSelectIcon}) => {
     <BottomSheetModal
       ref={bottomSheetRef}
       backgroundStyle={{backgroundColor: '#FFF'}}
-      snapPoints={['50%', '80%']}
+      // snapPoints={['50%', '50%']}
       maxDynamicContentSize={scale(400)}
       backdropComponent={props => (
         <Pressable
